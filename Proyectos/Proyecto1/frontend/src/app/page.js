a0fb3e023359333e0cccb76bd914715d23332846
DouglasSochC <3008474750101@ingenieria.usac.edu.gt>
2024-03-16 16:42:57 +0000

    [ADD] Implementacion de docker y NGINX completada

--- Proyectos/Proyecto1/frontend/src/app/page.js
+++ Proyectos/Proyecto1/frontend/src/app/page.js
@@ -1,7 +1,19 @@
 // @/pages/index.js
 import React from 'react'
-import DummyPage from '@/components/DummyPage'
+import Layout from '@/components/Layout/page';
 
 export default function Home() {
-  return <DummyPage title="Informacion" />
+  return (<>
+    <Layout
+      pageTitle=""
+    >
+      <div className="min-h-screen flex flex-col">
+        <div className="m-auto">
+          <h1 className="text-4xl"><b>Curso: </b>Sistemas Operativos 1</h1>
+          <h2 className="text-4xl"><b>Nombre: </b>Douglas Alexander Soch Catalán</h2>
+          <h2 className="text-4xl"><b>Carnet: </b>201807032</h2>
+        </div>
+      </div>
+    </Layout>
+  </>);
 }
